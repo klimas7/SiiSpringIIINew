f4a30935e141bd714f092e1cf9085d62cea324cb
klimas7 <klimas7@gmail.com>
2019-11-30 14:12:20 +0100

    Circular example #3: Break

--- core/src/main/java/pl/sii/spring/core/circular/BeanA.java
+++ core/src/main/java/pl/sii/spring/core/circular/BeanA.java
@@ -4,9 +4,16 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class BeanA {
+    private String message = "BeanA";
     private BeanB beanB;
 
     public BeanA(BeanB beanB) {
         this.beanB = beanB;
+        this.beanB.printMessage();
+
+    }
+
+    public void printMessage() {
+        System.out.println(message);
     }
 }
--- core/src/main/java/pl/sii/spring/core/circular/BeanB.java
+++ core/src/main/java/pl/sii/spring/core/circular/BeanB.java
@@ -5,9 +5,14 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class BeanB {
+    private String message = "BeanB";
     private BeanA beanA;
 
     public BeanB(@Lazy BeanA beanA) {
         this.beanA = beanA;
+        this.beanA.printMessage();
+    }
+    public void printMessage() {
+        System.out.println(message);
     }
 }
