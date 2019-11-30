383a4e6361212a2d8e2ec97a4ddc8d545b8721b6
klimas7 <klimas7@gmail.com>
2019-11-30 11:27:02 +0100

    annotation configuration #2: Spring config in class

--- core/src/main/java/pl/sii/spring/core/annotation/MainClass.java
+++ core/src/main/java/pl/sii/spring/core/annotation/MainClass.java
@@ -1,7 +1,15 @@
 package pl.sii.spring.core.annotation;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.Arrays;
+
 public class MainClass {
     public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext("pl.sii.spring.core.annotation");
 
+        System.out.println(context.getBeanDefinitionCount());
+        Arrays.asList(context.getBeanDefinitionNames()).forEach(System.out::println);
     }
 }
---
+++ core/src/main/java/pl/sii/spring/core/annotation/SpringConfig.java
@@ -0,0 +1,7 @@
+package pl.sii.spring.core.annotation;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+}
