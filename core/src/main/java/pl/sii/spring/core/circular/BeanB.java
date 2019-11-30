e69925c38f531b08f4f08643aefa763e07248516
klimas7 <klimas7@gmail.com>
2019-11-30 14:09:20 +0100

    Circular example #2: First solution

--- core/src/main/java/pl/sii/spring/core/circular/BeanB.java
+++ core/src/main/java/pl/sii/spring/core/circular/BeanB.java
@@ -1,12 +1,13 @@
 package pl.sii.spring.core.circular;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BeanB {
     private BeanA beanA;
 
-    public BeanB(BeanA beanA) {
+    public BeanB(@Lazy BeanA beanA) {
         this.beanA = beanA;
     }
 }
