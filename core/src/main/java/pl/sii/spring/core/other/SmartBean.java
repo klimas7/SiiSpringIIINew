c06737e7e44197ccfd5e986c9d35049c35b4078e
klimas7 <klimas7@gmail.com>
2019-11-30 12:58:05 +0100

    Other example #3: Inject with constructor

--- core/src/main/java/pl/sii/spring/core/other/SmartBean.java
+++ core/src/main/java/pl/sii/spring/core/other/SmartBean.java
@@ -2,19 +2,35 @@ package pl.sii.spring.core.other;
 
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
+import java.util.Arrays;
+import java.util.List;
 
 @Component
 public class SmartBean implements InitializingBean, DisposableBean {
-    private User user;
+    private List<User> users;
     private User otherUser;
 
-    public SmartBean(User user) {
-        this.user = user;
-        System.out.println("User: " + user);
+
+//    public SmartBean(User user) {
+//        this.users = Arrays.asList(user);
+//        System.out.println("User: " + user);
+//    }
+//
+//    @Autowired
+//    public SmartBean(User user, User user2) {
+//        this.users = Arrays.asList(user, user2);
+//        System.out.println("User: " + user);
+//    }
+
+
+    public SmartBean(List<User> users) {
+        this.users = users;
+        System.out.println("User: " + users);
     }
 
     @PostConstruct
